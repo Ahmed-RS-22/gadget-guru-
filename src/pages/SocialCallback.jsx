07d39bbfc914178919e87e9b0c8bdee9a752940e
Ahmed RS <111743227+Ahmed-RS-22@users.noreply.github.com>
2025-07-02 03:49:12 +0300

    Update SocialCallback.jsx

--- src/pages/SocialCallback.jsx
+++ src/pages/SocialCallback.jsx
@@ -1,22 +1,32 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 export default function SocialCallback({ onLogin }) {
   const navigate = useNavigate();
   const { search } = useLocation();
-
+  const [userInfo, setUserInfo] = useState({
+    token:"",
+    isUserLoggedIn: false,
+  })
   useEffect(() => {
     const params = new URLSearchParams(search);
     const token = params.get('token');
-    console.log('Token from URL:', token);
     if (token) {
-      localStorage.setItem("userInfo", JSON.stringify(token));
+      setUserInfo({
+        token: token || "",
+        isUserLoggedIn:true,
+      });
+
+      localStorage.setItem(
+        'userInfo',
+        JSON.stringify(userInfo)
+      );
       onLogin();
       navigate('/home');
     } else {
       navigate('/login');
     }
-  }, [search, navigate, onLogin]);
+  }, [search, navigate, onLogin,userInfo]);
 
   return <p>Finishing login...</p>;
 }
